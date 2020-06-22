f3bd96c6a43ea49b44f46bf2636368e3d49efd5e
hiteple <hiteplegold@gmail.com>
2020-06-21 22:48:59 -0300

    Adding custom validation on genre model

--- Entities/Genre.cs
+++ Entities/Genre.cs
@@ -1,13 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using MoviesAPI.Validations;
 
 namespace MoviesAPI.Entities
 {
-    public class Genre
+    public class Genre: IValidatableObject
     {
         public int Id { get; set; }
         
         [Required(ErrorMessage = "The field with name {0} is required")]
         [StringLength(10)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
 
         [Range(18, 120)]
@@ -18,5 +22,18 @@ namespace MoviesAPI.Entities
         
         [Url]
         public string Url { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be uppercase", new[] { nameof(Name) });
+                }
+            }
+        }
     }
 }
